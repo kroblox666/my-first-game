62dbbf5d0dce0e7d037a57515afa779762304617
kroblox666 <glebselyuk14@gmail.com>
2026-04-09 19:07:06 +1000

    Update firstgame.py

--- firstgame.py
+++ firstgame.py
@@ -1,31 +1,56 @@
 import pygame
-
+import random
 pygame.init()
 
 WIDTH = 400
 HEIGTH = 500
+enemies = []
 
-screen = pygame.display.set_mode((WIDTH,HEIGTH))
+for i in range(5):
+    x = random.randint(0, WIDTH-40)
+    y = random.randint(-800, -40)
+    enemies.append(pygame.Rect(x, y, 50, 50))
+    
+screen = pygame.display.set_mode((WIDTH, HEIGTH))
 pygame.display.set_caption("потм будет название")
 
 player = pygame.Rect(180, 450, 50, 50)
 
 clock = pygame.time.Clock()
 
-while True:
-  for events in pygame.event.get():
-    if events.type == pygame.QUIT:
-      exit()
-      
-  keys = pygame.key.get_pressed()
-  
-  if keys[pygame.K_LEFT]:
-    player.x -= 1
-  if keys[pygame.K_RIGHT]:
-    player.x += 1
+running = True
+
+while running:
+    for events in pygame.event.get():
+        if events.type == pygame.QUIT:
+            exit()
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        player.x -= 9
+    if keys[pygame.K_RIGHT]:
+        player.x += 9
+        
+    if player.right > WIDTH:
+        player.right = WIDTH
+    if player.left < 0:
+        player.left = 0
+        
+        
+    clock.tick(60)
+    screen.fill("green")
+
+    pygame.draw.rect(screen, "black", player)
     
-  clock.tick(60)
-  screen.fill("green")
-  
-  pygame.draw.rect(screen, "black", player)
-  pygame.display.update()
+    for enemy in enemies:
+        enemy.y += 9
+        if enemy.y > HEIGTH:
+            enemy.y = -50
+            enemy.x = random.randint(0, WIDTH-player.width)
+        pygame.draw.rect(screen, "white", enemy)
+        
+        if player.colliderect(enemy):
+            running = False
+    
+    pygame.display.update()
